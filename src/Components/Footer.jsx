71c92a329a37e5323e21e8af3b706cfcd31bd04e
Arpit <alfabatforarpitjain@gmail.com>
2024-07-27 11:41:00 +0530

    minor bug fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,109 +1,111 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import "./index.css";
 
 function Footer() {
-    return (
-        <section className="relative overflow-hidden py-10  text-white border-t-red-800 border-l-red-800 border-b-black border-r-black border-2 rounded-lg">
-            <div className="relative z-10 mx-auto max-w-7xl px-4">
-                <div className="-m-6 flex flex-wrap">
-                    <div className="w-full p-6 md:w-1/2 lg:w-5/12">
-                        <div className="flex h-full flex-col justify-between">
-                            <div className="mb-4 inline-flex items-center">
-                                <Logo width="100px" />
-                            </div>
-                            <div>
-                                <p className="text-sm  text-white">
-                                    &copy; Copyright 2023. All Rights Reserved by DevUI.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">
-                                Company
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Features
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Pricing
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Affiliate Program
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Press Kit
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-2/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">
-                                Support
-                            </h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Account
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Help
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Contact Us
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Customer Support
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="w-full p-6 md:w-1/2 lg:w-3/12">
-                        <div className="h-full">
-                            <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">Legals</h3>
-                            <ul>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium  text-white" to="/">
-                                        Terms &amp; Conditions
-                                    </Link>
-                                </li>
-                                <li className="mb-4">
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Privacy Policy
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link className=" text-base font-medium text-white" to="/">
-                                        Licensing
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="relative overflow-hidden py-10  text-white border-t-red-800 border-l-red-800 border-b-black border-r-black border-2 rounded-lg">
+      <div className="relative z-10 mx-auto max-w-7xl px-4">
+        <div className="-m-6 flex flex-wrap">
+          <div className="w-full p-6 md:w-1/2 lg:w-5/12">
+            <div className="flex h-full flex-col justify-between">
+              <div className="mb-4 inline-flex items-center">
+                <Logo width="100px" />
+              </div>
+              <div>
+                <p className="text-sm  text-white">
+                  &copy; Copyright 2023. All Rights Reserved by Ritik.
+                </p>
+              </div>
             </div>
-        </section>
-    );
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">
+                Company
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Features
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Pricing
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Affiliate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Press Kit
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">
+                Support
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Account
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Help
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Customer Support
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full p-6 md:w-1/2 lg:w-3/12">
+            <div className="h-full">
+              <h3 className="tracking-px mb-9  text-lg font-semibold uppercase text-slate-950">
+                Legals
+              </h3>
+              <ul>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium  text-white" to="/">
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link className=" text-base font-medium text-white" to="/">
+                    Licensing
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
